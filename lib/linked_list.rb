0ceb07b3ae2a4341a7d8c8f135c110d4662b612d
Will Weston <Williamweston81@gmail.com>
2023-07-10 13:02:12 -0500

    Weston(1) Linked_list.rb Create Linked list class w/ append method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,2 +1,25 @@
+require "./lib/node"
+
 class LinkedList
+    attr_accessor :head
+
+    def intialize
+        @head = nil
+    end
+
+    # append adds new piece of data to the end of the linked list
+    def append(data)
+
+        if @head.nil?
+
+            @head = Node.new(data, nil)
+        else
+            last_node = @head
+            while(!last_node.next_node.nil?)
+                last_node = last_node.next_node
+            end
+
+            last_node.next_node = Node.new(data, nil)
+        end
+    end
 end
