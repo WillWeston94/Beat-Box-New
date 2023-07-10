a4a89bf0ab97b8c42c2c83a07f252966d3c07137
Will Weston <Williamweston81@gmail.com>
2023-07-10 12:52:35 -0500

    Weston(1) node.rb Create node class

--- lib/node.rb
+++ lib/node.rb
@@ -1,2 +1,10 @@
 class Node
+    # allows us to get and set data, and next_node 
+    attr_accessor :data,
+                  :next_node
+
+    def initialize(data,next_node)
+        @data = data
+        @next_node = next_node
+    end
 end
