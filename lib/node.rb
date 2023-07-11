7741b9ad23efc5e76f648dee145019c89807da2c
Will Weston <Williamweston81@gmail.com>
2023-07-11 12:31:18 -0500

    Weston(1) Node code Add comment structure for code housekeeping

--- lib/node.rb
+++ lib/node.rb
@@ -1,13 +1,22 @@
-class Node
-    # allows us to get and set data, and next_node 
+class Node # represents node in linked list data structure 
+    # accessor provides getter and setter methods for data, and next_node
+    # allowing to read and modify values outside of their class
     attr_accessor :data,
                   :next_node
 
+    # data parameter represents value stored in the node
+    # next_node parameter represents next node in list
+    # next_node by default set to nil
+    # @data + @next_node assign values passed to initialize to instance variables to store our data
     def initialize(data,next_node = nil)
         @data = data
         @next_node = next_node
     end
 
+    # tail? returns boolean value 
+    # checks if current_node is our tail by checking next
+    # returns true if node = tail
+    # returns false if not
     def tail?
         next_node.nil?
     end
